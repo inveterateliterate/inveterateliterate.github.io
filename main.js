5c823341d441e22a780c2c5d80ca549d1d0f8251
inveterateliterate <iribon9@gmail.com>
2016-08-05 09:34:21 -0500

    creating main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){  
+  
+ 
+      $("nav a").click(function(evn){
+        evn.preventDefault();
+        $('html,body').animate({
+          scrollTop: $(this.hash).offset().top-100}, 1200
+        );
+    });
+  
+(function($){ 
+ 
+    var $body,
+    $target,
+    targetoffsetTop,
+    resizetimer,
+    stickyclass= 'sticky'
+     
+    function updateCoords(){
+        targetoffsetTop = $target.offset().top
+    }
+     
+    function makesticky(){
+        var scrollTop = $(document).scrollTop()
+        if (scrollTop >= targetoffsetTop){
+            if (!$body.hasClass(stickyclass)){
+                $body.addClass(stickyclass)
+            }
+        }
+        else{
+            if ($body.hasClass(stickyclass)){
+                $body.removeClass(stickyclass)
+            }
+        }
+    }
+     
+    $(window).on('load', function(){
+        $body = $(document.body)
+        $target = $('#header')
+        updateCoords()
+        $(window).on('scroll', function(){
+            requestAnimationFrame(makesticky)
+        })
+        $(window).on('resize', function(){
+            clearTimeout(resizetimer)
+            resizetimer = setTimeout(function(){
+                $body.removeClass(stickyclass)
+                updateCoords()
+                makesticky()
+            }, 50)
+        })
+    })
+ 
+})(jQuery)
+  
+});
